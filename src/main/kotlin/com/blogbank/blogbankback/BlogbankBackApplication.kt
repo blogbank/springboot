173e628fd51c9c69c2e71f72dc261cad036176a2
JintaTaekLim <jintaek019@gmail.com>
2025-12-19 16:43:09 +0900

    init

---
+++ src/main/kotlin/com/blogbank/blogbankback/BlogbankBackApplication.kt
@@ -0,0 +1,11 @@
+package com.blogbank.blogbankback
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BlogbankBackApplication
+
+fun main(args: Array<String>) {
+    runApplication<BlogbankBackApplication>(*args)
+}
---
+++ src/test/kotlin/com/blogbank/blogbankback/BlogbankBackApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.blogbank.blogbankback
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BlogbankBackApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
